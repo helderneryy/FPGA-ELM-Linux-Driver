dc044a0f24e49e8507485047e4fb2c35107ec2c6
Kiara Alencar <kiaralalencar@gmail.com>
2026-05-11 16:51:15 -0300

    adicionando PIO

---
+++ src/top_marco2.v
@@ -0,0 +1,20 @@
+module top_marco2 (
+
+	// Pinos da FPGA
+	input CLOCK_50,
+	output [9:0] LEDR,
+	input [3:0] KEY,
+	input [9:0] SW,
+	output [6:0] HEX0,
+	output [6:0] HEX1,
+	output [6:0] HEX2,
+	output [6:0] HEX3,
+	output [6:0] HEX4,
+	output [6:0] HEX5
+	
+	// Pinos do HPS
+	
+
+);
+
+endmodule
